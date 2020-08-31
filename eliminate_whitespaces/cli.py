5ad5a76ab497f06bd1da05b9362614c07871c8d4
Patrik Hlobil <patrik.hlobil@dm.de>
2020-08-31 21:00:38 +0200

    minor update

--- eliminate_whitespaces/cli.py
+++ eliminate_whitespaces/cli.py
@@ -2,25 +2,28 @@ from pathlib import Path
 import sys
 import click
 
-from eliminate_whitespaces.eliminare_whitespaces import (
+from .eliminare_whitespaces import (
     eliminate_newlines_after_function_definition_in_file_or_directory,
 )
 
 
 @click.command()
 @click.argument(
-    "path", required=True,
+    "path",
+    required=True,
 )
 @click.option(
-    "--check", help="Don't write the files back, just return the status.", is_flag=True,
+    "--check",
+    help="Don't write the files back, just return the status.",
+    is_flag=True,
 )
 def cli(path, check):
-    """This CLI formats Python code in such a way that after the function definition 
+    """This CLI formats Python code in such a way that after the function definition
     header all newlines will be deleted.
-    
+
     Return code 0 means nothing would change.  Return code 1 means some files would be reformatted. Return code 123 means there was an internal error.""
     pass.
-    
+
     Passed PATH can be either a file or a directory. In the latter case, all files in the
     folder will be formatted recursively.
     """
